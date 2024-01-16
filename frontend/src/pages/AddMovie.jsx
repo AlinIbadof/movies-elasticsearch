fefd59a2c5a4663ffb667d2b177fea8d9f139d62
Alin Ibadof <alinibadof@gmail.com>
2024-01-16 15:47:35 +0200

    refactor AddMovie using new endpoint

--- frontend/src/pages/AddMovie.jsx
+++ frontend/src/pages/AddMovie.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
 function AddMovie() {
+  const API_URL = "http://localhost:3001";
   const [title, setTitle] = useState("");
   const [year, setYear] = useState("");
   const [cast, setCast] = useState("");
   const [genres, setGenres] = useState("");
+  const [success, setSuccess] = useState();
 
   const handleAddMovie = async () => {
     try {
-      const response = await fetch("http://localhost:3001/api", {
+      const response = await fetch(`${API_URL}/api/movie`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,56 +27,76 @@ function AddMovie() {
       const data = await response.json();
       console.log(data);
 
+      setSuccess(true);
       // Handle success or display error message
     } catch (error) {
       console.error("Failed to add movie:", error);
+      setSuccess(false);
     }
   };
 
   return (
-    <div className="">
-      <h2>Add a Movie</h2>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Title:
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Year:
-          <input
-            type="number"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Cast:
-          <input
-            type="text"
-            value={cast}
-            onChange={(e) => setCast(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Genres:
-          <input
-            type="text"
-            value={genres}
-            onChange={(e) => setGenres(e.target.value)}
-          />
-        </label>
-        <br />
-        <Button variant="primary" onClick={handleAddMovie}>
-          Add Movie
-        </Button>
-      </form>
+    <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
+      <div className="text-center p-4">
+        <h2>Add a Movie</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="d-flex gap-2 mb-3 justify-content-center align-items-center">
+            <label htmlFor="title" className="form-label">
+              Title:
+            </label>
+            <input
+              type="text"
+              id="title"
+              className="form-control forminput"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-2 mb-3 justify-content-center align-items-center">
+            <label htmlFor="year" className="form-label">
+              Year:
+            </label>
+            <input
+              type="number"
+              id="year"
+              className="form-control forminput"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-2 mb-3 justify-content-center align-items-center">
+            <label htmlFor="cast" className="form-label">
+              Cast:
+            </label>
+            <input
+              type="text"
+              id="cast"
+              className="form-control forminput"
+              value={cast}
+              onChange={(e) => setCast(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-2 mb-3 align-items-center">
+            <label htmlFor="genres" className="form-label">
+              Genre:
+            </label>
+            <input
+              type="text"
+              id="genres"
+              className="form-control forminput"
+              value={genres}
+              onChange={(e) => setGenres(e.target.value)}
+            />
+          </div>
+          <Button variant="primary" onClick={handleAddMovie}>
+            Add Movie
+          </Button>
+        </form>
+      </div>
+      {success === true && (
+        <p className="text-success">Movie added succesfully</p>
+      )}
+      {success === false && <p className="text-danger">Failed to add movie</p>}
     </div>
   );
 }
